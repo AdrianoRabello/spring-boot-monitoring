ed78d48d0293331fd7b9eea86a8cbaa288fb8de6
adrianorabello <adrianor.rabello@hotmail.com>
2021-01-18 22:14:58 -0300

    application.yml for monitoring change for work with docker

--- src/main/java/br/com/springboot/monitoring/MonitoringApplication.java
+++ src/main/java/br/com/springboot/monitoring/MonitoringApplication.java
@@ -1,6 +1,7 @@
 package br.com.springboot.monitoring;
 
 import de.codecentric.boot.admin.server.config.EnableAdminServer;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -9,10 +10,15 @@ import org.springframework.context.annotation.Configuration;
 @Configuration
 @EnableAutoConfiguration
 @EnableAdminServer
-public class MonitoringApplication {
+public class MonitoringApplication implements CommandLineRunner {
 
     public static void main(String[] args) {
         SpringApplication.run(MonitoringApplication.class, args);
     }
 
+    @Override
+    public void run(String... args) throws Exception {
+
+        System.out.printf("initializing Monitoring application ");
+    }
 }
